5b95e3202c25a2e8cbb7789fd5786bc3296a2761
siglea <siglea@sigleadeiMac.local>
2026-04-05 13:18:34 +0800

    Revert "chore: 完成 OpenClaw 状态检查"
    This reverts commit fc1c4732eae3b307ba8c1fee82b90dbb1a19e292.

--- extensions/openclaw-cozeloop-trace/dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-import type { OpenClawPlugin } from "./types.js";
-declare const cozeloopTracePlugin: OpenClawPlugin;
-export default cozeloopTracePlugin;
-//# sourceMappingURL=index.d.ts.map
--- extensions/wecom-openclaw-plugin/dist/src/version.d.ts
+++
@@ -1,2 +0,0 @@
-/** 插件版本号，来源于 package.json */
-export declare const PLUGIN_VERSION: string;
